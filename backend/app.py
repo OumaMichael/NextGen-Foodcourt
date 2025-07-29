17dae65cb4a690de5cfe7b1d253915055f1e1bc5
Larry <larryotieno51@gmail.com>
2025-07-29 23:33:53 +0300

    Fixed render deployment issue

--- backend/app.py
+++ backend/app.py
@@ -7,7 +7,7 @@ from flask_jwt_extended import jwt_required, create_access_token, get_jwt_identi
 from datetime import datetime, time
 
 
-from config import app, db, api, jwt, jwt_blacklist
+from backend.config import app, db, api, jwt, jwt_blacklist
 from models import User, Cuisine, Outlet, MenuItem, Table, Order, OrderItem, Reservation
 
 @app.route('/')
